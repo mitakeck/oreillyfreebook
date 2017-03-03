824c49ad083c58dbdafa5256bd64844014b3627b
mitakeck <mitake.ck@gmail.com>
2017-03-03 19:10:00 +0900

    show download progress

--- utils.go
+++ utils.go
@@ -1,7 +1,13 @@
 package main
 
+import "strings"
+
 func pop(slice []string) (string, []string) {
 	ans := slice[len(slice)-1]
 	slice = slice[:len(slice)-1]
 	return ans, slice
+}
+
+func isContain(s string, substr string) bool {
+	return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
 }
